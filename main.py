4839da7d80229b6180abeea4ff56c5c4b398a93f
IDhammaI <1518470138@qq.com>
2024-06-18 13:50:35 +0800

    重构

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# 先到 https://www.mouser.cn/electronic-components/ dump所有产品的类别 包括产品名字 数量 产品网址路径
+from util.category_extractor.category_extractor import CategoryExtractor
+
+
+def main():
+    html_file_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\test_html\mouser_all.html'  # 输入 HTML 文件路径
+    output_json_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\json\category_data.json'  # 输出 JSON 文件路径
+
+    extractor = CategoryExtractor(html_file_path, output_json_path)
+    extractor.run()
+
+
+if __name__ == "__main__":
+    main()
