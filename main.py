fda2f9210efc75a7b0d5dbc3a8a4221a46b3c04f
IDhammaI <1518470138@qq.com>
2024-06-18 13:57:54 +0800

    TODO

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from util.category_extractor.category_extractor import CategoryExtractor
 
 
 def main():
-    html_file_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\test_html\mouser_all.html'  # 输入 HTML 文件路径
-    output_json_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\json\category_data.json'  # 输出 JSON 文件路径
+    # 输入 HTML 文件路径
+    html_file_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\test_html\mouser_all.html'
+    # 输出 JSON 文件路径
+    output_json_path = r'C:\Users\IDhammaI\PycharmProjects\maoser-spider\json\category_data.json'
 
+    # 更新元数据，保存本地json文件夹
     extractor = CategoryExtractor(html_file_path, output_json_path)
     extractor.run()
+
+    # TODO: 根据json文件夹下的 category_data.json 进行request请求，爬取html结构解析该类别的 ①图像url,②零件编号,③制造商,④数据表url,⑤供货情况,⑥单价 ....
+
+
+
+    # TODO: 异常处理 --> 更换代理IP
+
 
 
 if __name__ == "__main__":
